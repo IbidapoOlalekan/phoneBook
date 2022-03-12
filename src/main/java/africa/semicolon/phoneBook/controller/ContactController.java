10b24d9ec0dc0d3569ec086c3150dc0b735f77b4
Ibidapo Abdulazeez Olalekan <ibidapoazeez@gmail.com>
2022-03-12 20:12:10 +0100

    Updated Code

---
+++ src/main/java/africa/semicolon/phoneBook/controller/ContactController.java
@@ -0,0 +1,25 @@
+package africa.semicolon.phoneBook.controller;
+
+import africa.semicolon.phoneBook.dtos.requests.AddContactRequest;
+import africa.semicolon.phoneBook.dtos.responses.AddContactResponse;
+import africa.semicolon.phoneBook.dtos.responses.FindUserResponse;
+import africa.semicolon.phoneBook.services.AddContactService;
+import africa.semicolon.phoneBook.services.AddContactServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contact")
+public class ContactController {
+    private AddContactService addContactService = new AddContactServiceImpl();
+
+    @PostMapping("/register")
+    public AddContactResponse addNewContact(@RequestBody AddContactRequest requests){
+        return addContactService.save(requests);
+    }
+
+    @GetMapping("/{name}")
+    public FindUserResponse findUserByName(@PathVariable String name){return addContactService.findUserByName(name);}
+
+    @GetMapping("/{mobile}")
+    public FindUserResponse findUserByMobile(@PathVariable String mobile){return addContactService.findUserByMobile(mobile);}
+}
